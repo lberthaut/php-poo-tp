e23c51adb5a04b6de711ef399bf2b74ddaea1f6b
Fahari Hamada Sidi <mkl.devops@gmail.com>
2022-04-07 22:58:36 +0100

    Les accesseurs et mutateurs

--- cours/index.php
+++ cours/index.php
@@ -9,8 +9,29 @@ class Voiture
     {
         echo 'Voiture '.$this->carburant.', roulant à '.$this->vitesse.' KM/h';
     }
+
+    public function setVitesse($vitesse)
+    {
+        $this->vitesse = $vitesse;
+    }
+    public function getVitesse()
+    {
+        return $this->vitesse;
+    }
+    public function setCarburant($carburant)
+    {
+        if(in_array($carburant, ['diesel', 'essence', 'ethanol'])) {
+            $this->carburant = $carburant;
+        }
+    }
+    public function getCarburant()
+    {
+        return $this->carburant;
+    }
 }
 
 $mercedes = new Voiture();
+$mercedes->setCarburant('essence');
+$mercedes->setVitesse(120);
 $mercedes->rouler();
 
