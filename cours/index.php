ae7ba9abe8beada3baa30749867bad9f1d662037
Fahari Hamada Sidi <mkl.devops@gmail.com>
2022-04-07 21:49:11 +0100

    Propriétés

--- cours/index.php
+++ cours/index.php
@@ -2,28 +2,14 @@
 
 class Voiture
 {
-
+    public $vitesse = 100;
+    public $carburant = 'diesel';
 }
 
 $mercedes = new Voiture();
-
 var_dump($mercedes);
 
-function foo(&$var)
-{
-    $var = 2;
-}
-
-$a = 1;
-foo($a);
-
-// $a vaut 2 maintenant
-var_dump(compact('a'));
-
-$date1 = new DateTime();
-$date2 = $date1;
-$date3 = clone $date1;
-$date2->modify('+1 day');
-
-var_dump(compact('date1', 'date2', 'date3'));
+$mercedes->vitesse = 120;
+$mercedes->carburant = 'ethanol';
+var_dump($mercedes);
 
