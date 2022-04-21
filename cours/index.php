ba77c7600d44cbf1573c5621f5b8cd3afcc1ebbc
Fahari Hamada Sidi <mkl.devops@gmail.com>
2022-04-21 13:28:55 +0100

    add constructor

--- cours/index.php
+++ cours/index.php
@@ -5,6 +5,15 @@ class Voiture
     private $vitesse = 100;
     private $carburant = 'diesel';
 
+    public function __construct($vitesse = null, $carburant = null)
+    {
+      if(null !== $vitesse) {
+        $this->vitesse = $vitesse;
+      }
+      
+      $this->setCarburant($carburant);
+    }
+
     public function rouler()
     {
         echo 'Voiture '.$this->carburant.', roulant à '.$this->vitesse.' KM/h';
@@ -14,24 +23,27 @@ class Voiture
     {
         $this->vitesse = $vitesse;
     }
+
     public function getVitesse()
     {
         return $this->vitesse;
     }
+
     public function setCarburant($carburant)
     {
         if(in_array($carburant, ['diesel', 'essence', 'ethanol'])) {
             $this->carburant = $carburant;
         }
     }
+
     public function getCarburant()
     {
         return $this->carburant;
     }
 }
 
-$mercedes = new Voiture();
-$mercedes->setCarburant('essence');
-$mercedes->setVitesse(120);
-$mercedes->rouler();
+  $mercedes = new Voiture();
+  $scenic = new Voiture(90, 'ethanol');
+
+  var_dump(compact('scenic', 'mercedes'));
 
